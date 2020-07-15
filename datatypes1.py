f10020213ec6a085125912e8971f361561348135
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-07-15 20:51:06 +0530

    Add files via upload
    Here, in the first Data Types section in Python, we will see some data types like :
    Int, Float, String and Lists.
    
    Here, we will try to see the Memory Allocation of each variable and print the Data Type.

---
+++ datatypes1.py
@@ -0,0 +1,26 @@
+x = 80
+y = 20
+
+add = x+y
+
+print(add)
+
+print(type(x), type(y))
+
+print(id(x), id(y))
+
+a = 10.5
+print(a)
+print(type(a))
+
+
+s = "car"
+print(s)
+
+print(type(s))
+print(id(s))
+
+l = [10,20, "Hello", "Python"]
+print(l)
+print(type(l))
+print(id(l))
